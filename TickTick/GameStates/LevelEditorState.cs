7aa0ba5baaab62029c479bfffce5907407af120c
SteHun <61988201+SteHun@users.noreply.github.com>
2023-10-27 10:53:50 +0200

    Made level loading more flexible

--- TickTick/GameStates/LevelEditorState.cs
+++ TickTick/GameStates/LevelEditorState.cs
@@ -1,6 +1,29 @@
+using Engine;
+using Engine.UI;
+using Microsoft.Xna.Framework;
+
 namespace GameStates;
 
-public class LevelEditorState
+public class LevelEditorState : GameState
 {
-    
+    private Button saveButton;
+    private Button quitButton;
+
+    public LevelEditorState()
+    {
+        // add a "save" button
+        saveButton = new Button("Sprites/UI/spr_button_editor", 1);// temp
+        saveButton.LocalPosition = new Vector2(20, 20);
+        gameObjects.AddChild(saveButton);
+        
+        // add a "quit" button
+        quitButton = new Button("Sprites/UI/spr_button_quit", 1);
+        quitButton.LocalPosition = new Vector2(1290, 20);
+        gameObjects.AddChild(quitButton);
+    }
+
+    public override void HandleInput(InputHelper inputHelper)
+    {
+        
+    }
 }
