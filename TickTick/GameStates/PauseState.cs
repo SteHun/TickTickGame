50cb97bf4e425fc10ebc4916b9833fcc6a6a0434
SteHun <61988201+SteHun@users.noreply.github.com>
2023-11-12 18:25:36 +0100

    The game is now called Tick Tick: Boom!

--- TickTick/GameStates/PauseState.cs
+++ TickTick/GameStates/PauseState.cs
@@ -17,17 +17,6 @@ class PauseState : GameState
         
         // add a background
         gameObjects.AddChild(new UISpriteGameObject("Sprites/Backgrounds/spr_pause", TickTick.Depth_Background));
-
-        // add text box for pause
-        // UISpriteGameObject pauseTextBox = new UISpriteGameObject("Sprites/UI/spr_frame_text", TickTick.Depth_UIBackground);
-        // pauseTextBox.LocalPosition = new Vector2(720, 200);
-        // gameObjects.AddChild(pauseTextBox);
-        
-        // add pause text
-        // TextGameObject hintText = new TextGameObject("Fonts/HintFont", TickTick.Depth_UIForeground, Color.Black);
-        // hintText.Text = "Game is paused";
-        // hintText.LocalPosition = new Vector2(720, 200) + new Vector2(25, 20);
-        // gameObjects.AddChild(hintText);
         
         // add a "resume" button
         resumeButton = new Button("Sprites/UI/spr_button_quit", TickTick.Depth_UIForeground, "resume", "Fonts/MainFont");
